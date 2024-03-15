746c97570e26267f89bf8d2a8dc655c087af3a56
Ítalo Kauã N. D <122760006+ItaloKaua288@users.noreply.github.com>
2024-03-15 18:24:29 -0300

    Criação das aplicações em App

--- MPOO/Teste_de_Conhecimento/Semana4_TesteConhecimento/src/App.java
+++ MPOO/Teste_de_Conhecimento/Semana4_TesteConhecimento/src/App.java
@@ -1,4 +1,20 @@
 public class App {
     public static void main(String[] args) {
+        Conta conta1 = new Conta();
+        Cliente cliente3 = new Cliente("3", "333.333.333-33", 25);
+
+        Banco.depositar(BaseDados.getCliente1().getConta(), 100);
+        Banco.depositar(BaseDados.getCliente2().getConta(), 100);
+        System.out.println("Conta 1: " + BaseDados.getCliente1().getConta().getSaldo());
+        System.out.println("Conta 2: " + BaseDados.getCliente2().getConta().getSaldo());
+
+        Banco.sacar(BaseDados.getCliente1().getConta(), 10);
+        System.out.println("Conta 1: " + BaseDados.getCliente1().getConta().getSaldo());
+        System.out.println("Conta 2: " + BaseDados.getCliente2().getConta().getSaldo());
+
+        Banco.transferir(BaseDados.getCliente2().getConta(), BaseDados.getCliente1().getConta(), 10);
+        System.out.println("Conta 1: " + BaseDados.getCliente1().getConta().getSaldo());
+        System.out.println("Conta 2: " + BaseDados.getCliente2().getConta().getSaldo());
+
     }
 }
