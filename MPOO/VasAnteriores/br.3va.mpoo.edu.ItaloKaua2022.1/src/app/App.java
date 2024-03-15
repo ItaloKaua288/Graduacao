d513b6b55eebdfcb54ec2837389bbb07a8001a6a
Ítalo Kauã N. D <122760006+ItaloKaua288@users.noreply.github.com>
2024-03-15 19:28:59 -0300

    Update MensagemView

--- MPOO/VasAnteriores/br.3va.mpoo.edu.ItaloKaua2022.1/src/app/App.java
+++ MPOO/VasAnteriores/br.3va.mpoo.edu.ItaloKaua2022.1/src/app/App.java
@@ -1,8 +1,7 @@
 package app;
 
-import view.CadastroView;
-
 public class App {
     public static void main(String[] args) {
+
     }
 }
--- MPOO/VasAnteriores/br.3va.mpoo.edu.ItaloKaua2022.1/src/view/MensagemView.java
+++ MPOO/VasAnteriores/br.3va.mpoo.edu.ItaloKaua2022.1/src/view/MensagemView.java
@@ -1,7 +1,14 @@
 package view;
 
-import javax.swing.JPanel;
+import javax.swing.JOptionPane;
 
-public class MensagemView extends JPanel{
-    
+public class MensagemView extends JOptionPane{
+    public void exibirMensagem(String msg) {
+        showMessageDialog(null, msg, "Mensagem", JOptionPane.CLOSED_OPTION);
+    }
+
+    public void exibirMensagemErro(String msg) {
+        showMessageDialog(null, msg, "Mensagem", JOptionPane.ERROR_MESSAGE);
+    }
+
 }
