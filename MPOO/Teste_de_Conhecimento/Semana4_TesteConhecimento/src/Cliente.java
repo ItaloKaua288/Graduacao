3d678c90f8645ff2052387cd0fd9b9cde09cce44
Ítalo Kauã N. D <122760006+ItaloKaua288@users.noreply.github.com>
2024-03-15 17:58:59 -0300

    Update Cliente

--- MPOO/Teste_de_Conhecimento/Semana4_TesteConhecimento/src/Cliente.java
+++ MPOO/Teste_de_Conhecimento/Semana4_TesteConhecimento/src/Cliente.java
@@ -1,3 +1,25 @@
 public class Cliente {
+    private String nome;
+    public String cpf;
+    private int idade;
+    private Conta conta = new Conta();
+
+    public Cliente(String nome, String cpf, int idade) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public Conta getConta() {
+        return conta;
+    }
     
 }
