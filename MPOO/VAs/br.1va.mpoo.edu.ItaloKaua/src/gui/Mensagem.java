4d1f657a74128e48a96a91f2f2eca93ff813d380
Ítalo Kauã N. D <122760006+ItaloKaua288@users.noreply.github.com>
2024-03-14 21:36:26 -0300

    Criação do conteúdo de gui

---
+++ MPOO/VAs/br.1va.mpoo.edu.ItaloKaua/src/gui/Mensagem.java
@@ -0,0 +1,12 @@
+package gui;
+
+import javax.swing.JOptionPane;
+
+public class Mensagem {
+    public static final String PRODUTO_SUCESSO = "Produto cadastrado com sucesso!";
+    public static final String PRODUTO_ERRO = "Erro ao cadastrar produto";
+
+    public void exibirMensagem(String mensagem) {
+        JOptionPane.showMessageDialog(null, mensagem, "Mensagem", 1);
+    }
+}
