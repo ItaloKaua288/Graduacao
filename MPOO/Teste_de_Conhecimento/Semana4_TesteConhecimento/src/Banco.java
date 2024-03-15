cd746f2c045f50a04963870d65e0bacfe96a6c32
Ítalo Kauã N. D <122760006+ItaloKaua288@users.noreply.github.com>
2024-03-15 18:16:17 -0300

    Update Banco

--- MPOO/Teste_de_Conhecimento/Semana4_TesteConhecimento/src/Banco.java
+++ MPOO/Teste_de_Conhecimento/Semana4_TesteConhecimento/src/Banco.java
@@ -1,3 +1,22 @@
 public class Banco {
-    
+    public static void depositar(Conta conta, double valor) {
+        conta.setSaldo(conta.getSaldo() + valor);
+    }
+
+    public static boolean sacar(Conta conta, double valor) {
+        if (conta.getSaldo() >= valor) {
+            conta.setSaldo(conta.getSaldo() - valor);
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean transferir(Conta contaOrigem, Conta contaDestino, double valor) {
+        if (contaOrigem.getSaldo() < valor || contaOrigem == null || contaDestino == null)
+            return false;
+        
+        sacar(contaOrigem, valor);
+        depositar(contaDestino, valor);
+        return true;
+    }
 }
