9b1160fa0b7210d38c48482e982f0d156c70caad
Ítalo Kauã N. D <122760006+ItaloKaua288@users.noreply.github.com>
2024-03-15 18:06:49 -0300

    Update BaseDados

--- MPOO/Teste_de_Conhecimento/Semana4_TesteConhecimento/src/BaseDados.java
+++ MPOO/Teste_de_Conhecimento/Semana4_TesteConhecimento/src/BaseDados.java
@@ -1,3 +1,12 @@
 public class BaseDados {
+    private static Cliente cliente1 = new Cliente("1", "111.111.111-11",18);
+    private static Cliente cliente2 = new Cliente("2", "222.222.222-22", 19);
+    
+    public static Cliente getCliente1() {
+        return cliente1;
+    }
+    public static Cliente getCliente2() {
+        return cliente2;
+    }
     
 }
