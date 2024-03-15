5692cb257d8293d157238a8cb706bfe111a502ed
Ítalo Kauã N. D <122760006+ItaloKaua288@users.noreply.github.com>
2024-03-15 18:02:17 -0300

    Update Conta

--- MPOO/Teste_de_Conhecimento/Semana4_TesteConhecimento/src/Conta.java
+++ MPOO/Teste_de_Conhecimento/Semana4_TesteConhecimento/src/Conta.java
@@ -1,3 +1,32 @@
 public class Conta {
+    private int numero;
+    private static final int AGENCIA = 2730;
+    private double saldo;
+    private static int cont;
+
+    public Conta() {
+        this.numero = cont++;
+    }
+
+    public Conta(double saldo) {
+        this.saldo = saldo;
+        this.numero = cont++;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public static int getAgencia() {
+        return AGENCIA;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
     
 }
