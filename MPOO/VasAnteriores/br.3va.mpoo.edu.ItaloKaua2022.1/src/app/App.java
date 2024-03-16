2ceb4d5b5dc937052838c2b87ebf66e129313ea7
Ítalo Kauã N. D <122760006+ItaloKaua288@users.noreply.github.com>
2024-03-16 15:55:01 -0300

    Projeto de teste removido

--- MPOO/VasAnteriores/br.3va.mpoo.edu.ItaloKaua2022.1/src/app/App.java
+++ MPOO/VasAnteriores/br.3va.mpoo.edu.ItaloKaua2022.1/src/app/App.java
@@ -1,18 +1,24 @@
+import controller.CadastroController;
+import controller.LoginController;
+import controller.MenuController;
 import model.BaseDados;
+import model.SuperUsuario;
+import view.CadastroView;
+import view.LoginView;
+import view.MenuView;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println();''
         BaseDados.createBase();
 
-        // MenuView menuView = new MenuView();
-        // LoginView loginView = new LoginView();
-        // CadastroView cadastroView = new CadastroView();
+        MenuView menuView = new MenuView();
+        LoginView loginView = new LoginView();
+        CadastroView cadastroView = new CadastroView();
 
-        // new MenuController(menuView, cadastroView, loginView);
-        // new CadastroController(menuView, cadastroView);
-        // new LoginController(menuView, loginView);
+        new MenuController(menuView, cadastroView, loginView);
+        new CadastroController(menuView, cadastroView);
+        new LoginController(menuView, loginView);
 
-        // SuperUsuario SuperUsuario = new SuperUsuario();
+        SuperUsuario SuperUsuario = new SuperUsuario();
     }
 }
