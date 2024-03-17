917bfebe62e0c998d220ccf6d5d4fba5ccf37c99
Ítalo Kauã N. D <122760006+ItaloKaua288@users.noreply.github.com>
2024-03-17 14:43:25 -0300

    Criação da view AVencer

---
+++ MPOO/VasAnteriores/br.3va.mpoo.edu.ItaloKaua2021.2/src/sistemaBudega/view/AVencer.java
@@ -0,0 +1,31 @@
+package sistemaBudega.view;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.FlowLayout;
+
+public class AVencer extends JFrame{
+    private JLabel prazoLabel;
+    private JTextField prazoField;
+    private JButton buscarButton;
+
+    public AVencer() {
+        super("A Vencer");
+        setSize(190, 100);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new FlowLayout());
+
+        prazoLabel = new JLabel("Prazo (dias):");
+        prazoField = new JTextField(5);
+        buscarButton = new JButton("Buscar");
+
+        add(prazoLabel);
+        add(prazoField);
+        add(buscarButton);
+
+        setVisible(true);
+    }
+}
